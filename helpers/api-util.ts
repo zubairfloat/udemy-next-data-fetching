1013f545a8b4f369f23bc2877265a265564247fc
zubair <zubairfloat@gmail.com>
2023-01-02 00:41:59 +0500

    complete pre-rendring and data fetching

---
+++ helpers/api-util.ts
@@ -0,0 +1,42 @@
+export async function getAllEvents() {
+  const response = await fetch(
+    "https://nextjs-course-412da-default-rtdb.firebaseio.com/events.json"
+  );
+  const data = await response.json();
+
+  const events = [];
+
+  for (const key in data) {
+    events.push({
+      id: key,
+      ...data[key],
+    });
+  }
+
+  return events;
+}
+
+export async function getFeaturedEvents() {
+  const allEvents = await getAllEvents();
+  return allEvents.filter((event) => event.isFeatured);
+}
+
+export async function getEventById(id: any) {
+  const allEvents = await getAllEvents();
+  return allEvents.find((event) => event.id === id);
+}
+
+export async function getFilteredEvents(dateFilter: { year: any; month: any; }) {
+  const { year, month } = dateFilter;
+
+  const allEvents = await getAllEvents();
+
+  let filteredEvents = allEvents.filter((event) => {
+    const eventDate = new Date(event.date);
+    return (
+      eventDate.getFullYear() === year && eventDate.getMonth() === month - 1
+    );
+  });
+
+  return filteredEvents;
+}
