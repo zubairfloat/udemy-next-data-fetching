072be47febbc37a1fe6fc2a2eb12b835f41f889f
zubair <zubairfloat@gmail.com>
2023-02-07 01:44:54 +0500

    add simple notification

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import Layout from "../components/layout/layout";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Notification from "../components/ui/notification";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
+      <Notification title="Test" message="This is a test" status="success" />
     </Layout>
   );
 }
